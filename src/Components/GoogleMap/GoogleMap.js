0ebaf2ce75a985d7c6f332c5c8c953278ea7a6f3
IftekharPriyo <priyo.mobin123@gmail.com>
2020-09-18 20:39:39 +0600

    map api key fixed

--- src/Components/GoogleMap/GoogleMap.js
+++ src/Components/GoogleMap/GoogleMap.js
@@ -12,16 +12,14 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const GoogleMap = () => {
     const [place, setPlace] = useContext(PlaceContext);
 
-    const placeArray = placeDetails.filter(plc => parseInt(plc.key) === parseInt(place))
-    const lattitude = placeArray[0].lat
-    const longitude = placeArray[0].lng
-    console.log(lattitude,longitude);
-   
-    
+    const placeList = placeDetails.filter(placeD => parseInt(placeD.key) === parseInt(place))
+    const lattitude = placeList[0].lat
+    const longitude = placeList[0].lng
+
     return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDw0R0Hb_DiVfM0VgZLBr9ZwjowFwu8RSY' }}
+          bootstrapURLKeys={{ key: 'AIzaSyATz2kHmvXIjxVO0UqZeiZXizP-9qEZ_6s' }}
           defaultCenter={{lat:parseInt(lattitude),lng:parseInt(longitude)}}
           defaultZoom={10}
         >
